0ce04cffed1a2339ec7b1876d55bfc6cad8c53c5
devcebir <bardakcimehmetdev@gmail.com>
2023-12-05 02:24:58 +0300

    update

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,16 +1,14 @@
-"use client";
 import { GiBowArrow } from "react-icons/gi";
 import { IoSunnyOutline } from "react-icons/io5";
 import { PiMoonFill } from "react-icons/pi";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 w-full bg-white  flex items-center justify-between px-72">
+    <header className="sticky top-0 w-full bg-white  flex items-center justify-between px-56">
       <Link
         href="/"
-        className="flex items-center gap-3 hover:text-sky-950 transition-all duration-300 py-3"
+        className="flex items-center gap-3 hover:text-[#27374ddc] transition-all duration-300 py-3"
       >
         <GiBowArrow className="text-5xl" />
         <h1 className="text-3xl font-bold">EHL-İ TARİH</h1>
@@ -19,13 +17,13 @@ export default function Header() {
       <nav className="flex items-center gap-10 font-semibold">
         <Link
           href="/turk-tarihi"
-          className="hover:bg-transparent text-xl  hover:text-sky-950 transition-all duration-300"
+          className="hover:bg-transparent text-xl  hover:text-[#27374ddc] transition-all duration-300"
         >
           Türk Tarihi
         </Link>
         <Link
           href="/islam-tarihi"
-          className="hover:bg-transparent text-xl  hover:text-sky-950 transition-all duration-300"
+          className="hover:bg-transparent text-xl  hover:text-[#27374ddc] transition-all duration-300"
         >
           İslam Tarihi
         </Link>
--- components/page-detail/index.jsx
+++ components/page-detail/index.jsx
@@ -9,34 +9,23 @@ export default function PageDetail({ title, subtitle, href, src }) {
 
   return (
     <div
-      className="relative rounded-3xl overflow-hidden flex items-center justify-center"
+      className="relative  overflow-hidden flex items-center justify-center"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <Image
         src={src}
+        priority
         alt="Asya Hun Bayrak"
-        className={`rounded-3xl ${
+        className={` ${
           isHovered ? "transition-all brightness-50 duration-1000" : ""
         }`}
-        onLoad={(e) => {
-          const { naturalWidth, naturalHeight } = e.target;
-          const aspectRatio = naturalWidth / naturalHeight;
-
-          const container = e.target.parentNode;
-          container.style.width = "250px"; // Ajust this width as needed
-          container.style.height = `${250 / aspectRatio}px`; // Maintain aspect ratio
-
-          const overlay = container.querySelector(".overlay");
-          overlay.style.top = `50%`;
-          overlay.style.left = `50%`;
-        }}
       />
 
       {isHovered && (
         <Link
           href={href}
-          className="overlay text-center absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center  hover:bg-opacity-50 text-white rounded-3xl"
+          className="overlay text-center absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center  hover:bg-opacity-50 text-white"
         >
           <h3 className="text-2xl font-bold">{title}</h3>
           <p className="">{subtitle}</p>
