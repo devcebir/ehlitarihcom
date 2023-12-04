87266a4631b62ef1319e85614a3fc085b2a3bbff
devcebir <bardakcimehmetdev@gmail.com>
2023-12-05 02:03:41 +0300

    update header

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -6,22 +6,14 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Header() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
   return (
-    <header className="sticky top-0 w-full bg-white  flex items-center justify-between px-80">
+    <header className="sticky top-0 w-full bg-white  flex items-center justify-between px-72">
       <Link
         href="/"
         className="flex items-center gap-3 hover:text-sky-950 transition-all duration-300 py-3"
       >
         <GiBowArrow className="text-5xl" />
-        <h1 className={`text-3xl font-bold ${darkMode ? "text-white" : ""}`}>
-          EHL-İ TARİH
-        </h1>
+        <h1 className="text-3xl font-bold">EHL-İ TARİH</h1>
       </Link>
 
       <nav className="flex items-center gap-10 font-semibold">
@@ -39,14 +31,8 @@ export default function Header() {
         </Link>
 
         <div className="flex items-center gap-2 border border-zinc-200 rounded-full py-1 px-3">
-          <IoSunnyOutline
-            className="text-2xl transform hover:rotate-180 transition-all duration-1000 cursor-pointer"
-            onClick={toggleDarkMode}
-          />
-          <PiMoonFill
-            className="text-2xl transform hover:-rotate-180 transition-all duration-1000 cursor-pointer"
-            onClick={toggleDarkMode}
-          />
+          <IoSunnyOutline className="text-2xl transform hover:rotate-180 transition-all duration-1000 cursor-pointer" />
+          <PiMoonFill className="text-2xl transform hover:-rotate-180 transition-all duration-1000 cursor-pointer" />
         </div>
       </nav>
     </header>
